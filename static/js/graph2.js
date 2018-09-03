1717a917970828b921592586382131f2ab1fe3f7
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-09-03 19:10:25 +0000

    index2-discipline selector

---
+++ static/js/graph2.js
@@ -0,0 +1,39 @@
+queue()                                                                 
+    .defer(d3.csv, "data/Salaries.csv")         
+    .await(makeGraphs);                           
+
+function makeGraphs(error, salaryData) {        
+    var ndx = crossfilter(salaryData);
+    
+    show_discipline_selector(ndx);
+    show_gender_balance(ndx);
+    
+    dc.renderAll();
+}
+
+function show_discipline_selector(ndx) {
+    dim = ndx.dimension(dc.pluck("discipline"));
+    group = dim.group()
+    
+    dc.selectMenu("#discipline-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+function show_gender_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck("sex"));
+    var group = dim.group();
+    
+    dc.barChart("#gender-balance")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
+}
