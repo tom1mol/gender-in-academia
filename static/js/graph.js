5f8e444b577447cfe71553d36de3be629eb7461c
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-09-03 13:19:00 +0000

    discipline-selector

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,10 +6,24 @@ queue()                                                                 //to loa
 function makeGraphs(error, salaryData) {        //salaryData is a variable that data from csv file will be passed into by queue.js
     var ndx = crossfilter(salaryData);          //create crossfilter. 1 for whole dashboard and load salaryData into it
     
+    show_discipline_selector(ndx);      //pass this ndx as only argument
     show_gender_balance(ndx);       // we pass the ndx variable(crossfilter) to the function that will draw our graph
     
     dc.renderAll();
 }  
+
+
+function show_discipline_selector(ndx) {            // we call this from makeGraphs function above
+                                // select menus easy to render. just need dimension and group and pass them to dimensional charting select menu
+      dim = ndx.dimension(dc.pluck("discipline"));  //to create dimension we pluck discipline column from crossfilter
+      group = dim.group()
+      
+      dc.selectMenu("#discipline-selector")       //selectMenu needs to be told the div to render in..#discipline-selector(CSS selector pointing)
+        .dimension(dim)
+        .group(group);
+    
+}
+
 
 function show_gender_balance(ndx) {         //each graph has its own function. one argument(ndx). for each graph we have div,function + graph rendered in div
     var dim = ndx.dimension(dc.pluck("sex"));
@@ -25,7 +39,7 @@ function show_gender_balance(ndx) {         //each graph has its own function. o
         .x(d3.scale.ordinal())          //use ordinal scale because dimension consists of words male and female
                                         //y axis is count of how many of each there were
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
+        //.elasticY(true)  ..got rid of this. the bars didnt change because of it..just the y-axis
         .xAxisLabel("Gender")
         .yAxis().ticks(20);         //number of ticks that appear on y-axis
 }
